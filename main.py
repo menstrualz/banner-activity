f6d68eaaf8ba466980a63a579c39a99969605373
menstrualz <127664990+menstrualz@users.noreply.github.com>
2024-02-24 19:42:44 +0200

    new method start bot

--- main.py
+++ main.py
@@ -1,39 +1,28 @@
-# from tools.setuptool import bot
-
-
-# class Bot:
-#     pass
-
-# if __name__ == "__main__":
-#     print("Starting bot...")
-#     bot.run()
-
 import disnake
 from systems import setup
 from disnake.ext import commands
 from loguru import logger
 from configs.config import Config
 
-class Bot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix = "!",
-                         intents = disnake.Intents.all(),
-                         help_command = None,
-                         test_guilds=[1104144898157981760],
-                         command_sync_flags = commands.CommandSyncFlags.all(),
-                         reload = True,
-                         )
+bot = commands.Bot(command_prefix = "!",
+                intents = disnake.Intents.all(),
+                help_command = None,
+                test_guilds=[1104144898157981760],
+                reload = True,
+                )
 
-    async def on_ready(self):
-        logger.success(f"-> <DISCORD API  CONNECTED > {self.user.name} запущен")
+@bot.event
+async def on_ready():
+    logger.success(f"-> < DISCORD API  CONNECTED > {bot.user.name} запущен")
 
-    async def on_resumed(self):
-        logger.warning(f"-> < DISCORD API RESUMED > {self.user}")
+@bot.event
+async def on_resumed():
+    logger.warning(f"-> < DISCORD API RESUMED > {bot.user}")
 
-    async def on_disconnect(self):
-        logger.critical(f"-> < DISCORD API DISCONNECTED > {self.user}")
+@bot.event
+async def on_disconnect():
+    logger.critical(f"-> < DISCORD API DISCONNECTED > {bot.user}")
 
-bot = Bot()
 setup(bot)
 
 if __name__ == "__main__":
