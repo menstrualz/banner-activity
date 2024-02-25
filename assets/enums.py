4260c3bca865e6f35248569bad86c38d1dc1e256
menstrualz <127664990+menstrualz@users.noreply.github.com>
2024-02-25 16:15:47 +0200

    delete interval const

--- assets/enums.py
+++ assets/enums.py
@@ -13,4 +13,3 @@ class BannerConstants(Enum):
     MEMBER_TOTAL_POSITION = (576, 245) #позиция количества участников
     VOICE_TOTAL_POSITION = (853, 395) #позиция количества участников в голосовом канале
     ACTIVE_USER_NAME_POSITION = (477, 309) #позиция имени активного участника
-    BANNER_UPDATE_INTERVAL = 120  #в минутах
